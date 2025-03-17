47f22dbab75fe66dbf0eb608ba66b203d93a8246
paramet kongjaroen <parametprame2@gmail.com>
2025-03-17 16:08:19 +0700

    feat: support deploy token via factory contract

---
+++ src/mappings/erc-7818/factory.ts
@@ -0,0 +1,36 @@
+import { Factory, Token } from "../../../generated/schema";
+import { ERC7818 } from "../../../generated/templates";
+import { NewTokenContract } from "../../../generated/ERC7818Factory/TemplateERC20Factory";
+import { getOrCreateAccount } from "../erc-7818/account";
+import { ZERO } from "../../helpers/number";
+
+export function handleTokenDeployed(event: NewTokenContract): void {
+  let factory = Factory.load(event.address.toHex());
+
+  let account = getOrCreateAccount(event.params.deployer);
+
+  if (factory == null) {
+    factory = new Factory(event.address.toHex());
+    factory.address = event.address;
+    factory.save();
+  }
+
+  let token = new Token(event.params.tokenAddress.toHex());
+  token.address = event.params.tokenAddress;
+  token.totalSupply = ZERO.toBigDecimal();
+  token.totalTokenTransferred = ZERO.toBigDecimal();
+  token.totalTokenMinted = ZERO.toBigDecimal();
+  token.totalTokenBurned = ZERO.toBigDecimal();
+  token.totalEvent = ZERO;
+  token.totalEventTransfer = ZERO;
+  token.totalEventMint = ZERO;
+  token.totalEventBurn = ZERO;
+  token.createdAt = event.block.timestamp;
+  token.createdAtBlockNumber = event.block.number;
+  token.factoryAddress = factory.address.toHex();
+  token.creator = account.id;
+  token.save();
+
+  // Start indexing the new ERC-20 token
+  ERC7818.create(event.params.tokenAddress);
+}
